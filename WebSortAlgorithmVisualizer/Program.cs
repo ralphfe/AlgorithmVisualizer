499ec4c0656de18696b36e187dfb5ed959c24f39
Ralfs Fenuks <ralfs.fenuks@gmail.com>
2021-01-02 20:02:55 +0200

    Minor changes

--- WebSortAlgorithmVisualizer/Program.cs
+++ WebSortAlgorithmVisualizer/Program.cs
@@ -1,15 +1,17 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace WebSortAlgorithmVisualizer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
     public class Program
     {
         public static async Task Main(string[] args)
